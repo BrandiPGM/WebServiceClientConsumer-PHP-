7a3dfed00a16207b595695858e06983cdfbfe253
Mérillat Brandon <brandon.merillat@rpn.ch>
2017-01-30 16:19:45 +0100

    Ajout Classe debut Code

--- CSharp/WebServiceClientConsumer/MainWindow.xaml.cs
+++ CSharp/WebServiceClientConsumer/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HttpUtils;
+using Newtonsoft.Json;
 
 namespace WebServiceClientConsumer
 {
@@ -23,6 +25,38 @@ namespace WebServiceClientConsumer
         public MainWindow()
         {
             InitializeComponent();
+
+            cboLocalite.Items.Add("Neuchatel");
+            cboLocalite.Items.Add("La-chaux-de-fonds");
+            cboLocalite.Items.Add("Bern");
+            cboLocalite.Items.Add("Lausanne");
+
+            cboPrevision.Items.Add(1);
+            cboPrevision.Items.Add(2);
+            cboPrevision.Items.Add(3);
+            cboPrevision.Items.Add(4);
+            cboPrevision.Items.Add(5);
         }
+
+        private void GetCurrencyWeather(string Ville)
+        {
+            Meteo MeteoReponse = new Meteo();
+
+            try
+            {
+                string endPoint = @"http://www.prevision-meteo.ch/services/json/" + Ville;
+                var client = new RestClient(endPoint);
+                var json = client.MakeRequest();
+                object objResponse = JsonConvert.DeserializeObject(json, typeof(Meteo));
+
+                //COnverti dans le type requis
+                MeteoReponse = (Meteo)objResponse;
+            }
+            catch
+            {
+
+            }
+        }
+
     }
 }
